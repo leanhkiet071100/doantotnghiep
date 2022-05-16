52ebab3a57939fb067c3695c7a71b2facb3c129f
leanhkiet071100 <leanhkiet071100@gmail.com>
2022-05-16 09:25:02 +0700

    upload

---
+++ doantotnghiep/resources/views/dangnhap.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mạng xã hội cao thắng</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/dangnhap.css">
+</head>
+
+<body>
+    <div class="dangnhap">
+        <div class="wrap-content">
+            <div class="flex  justify-content-between">
+                <div class="hinh">
+                    <div class="hinhanh">
+                        <img src="/assets/images/hinhnendangnhap.png" alt="" width="513px" height="365px" >
+                    </div>
+                </div>
+                <div class="dang-nhap">
+                    <div class="mangxahoi banner">
+                           Login
+                    </div>
+                
+                    <form action="">
+                        <div class="taikhoan">
+                            <input type="text" class="input-taikhan" name="taikhoan" id="taikhoan"
+                                placeholder="Email address" require>
+                        </div>
+                        <div class="matkhau">
+                            <input type="text" class="input-matkhau" name="taikhoan" id="taikhoan" placeholder="Password"
+                                require>
+                        </div>
+                        <div class="remember forgot flex justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="Rememberme">
+                                <label class="form-check-label" for="Rememberme">
+                                    Remember me
+                                </label>
+                            </div>
+                            <a href="" class="quenmatkhau">Forgot password?</a>
+                        </div>
+                        <div class="button-dang-nhap">
+                            <button type="submit" class="btn btn-primary button-login">Login</button>
+                        </div>
+                    </form>
+                    <div class="bottom-dangnhap">
+                        <div class="flex justify-content-center">    <p>Don’t have account? </p> <a href="">Register</a></div>
+                     
+                        <p>or</p>
+                        <div  class="flex justify-content-center"> <p>Login with Google</p> <a href=""><img src="/assets/images/logogoogle.png" alt=""></a></div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
